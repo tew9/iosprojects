b83720396e02fa32239188c2852f036da9123a75
Tango <tango.tew@outlook.com>
2022-04-01 18:06:46 -0500

    add first project 'I am Rich'

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by user212411 on 4/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
